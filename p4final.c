880b22c8dfcf09a7cc4e3907884c899d10d2205d
BHARATH2145 <94664858+Bharath2145@users.noreply.github.com>
2022-03-21 06:23:14 +0000

    codded

--- p4final.c
+++ p4final.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
 
-int input_array_size()
+int input_number()
 {
   int n;
   printf("Enter size\n");
   scanf("%d",&n);
   return n;
 }
-
-void input_array(int n, int a[n])
-{
-  printf("Enter the array values\n");
-  for(int i=0;i<n;i++)
-  {
-    scanf("%d",&a[i]);
-  }
-}
-
 int is_composite(int n)
 {
   for(int i = 2; i <= n/2; i++)
--- p5final.c
+++ p5final.c
@@ -4,30 +4,23 @@ int input()
 {
   int a;
   printf("Enter the number\n");
-  scanf("%d%d",&a);
+  scanf("%d",&a);
   return a;
 }
-
-int find_gcd(int a, int b)
+int _gcd(int a, int b)
 {
-  int gcd,t;
-  if( a >b)
-  {
-    t = a;
-    a = b;
-    b = t;
-  }
-  if ( b%a == 0)
-    return a;
+  int gcd=1;
   for(int i=2; i<a/2; i++)
   {
-    if ( a%i==0 && b%i==0)
-      gcd = i;
+   if ( a%i==0 && b%i==0)
+   { 
+     gcd = i;
+    }
   }
   return gcd;
 }
 
-void output(int a, int b, int gcd)
+void out_put(int a, int b, int gcd)
 {
   printf("The gcd %d and %d is %d\n",a,b,gcd);
 }
@@ -37,7 +30,7 @@ int main()
   int a, b, gcd;
   a = input();
   b = input();
-  gcd = find_gcd(a,b);
-  output(a,b,gcd);
+  gcd =_gcd(a,b);
+  out_put(a,b,gcd);
   return 0;
 }
